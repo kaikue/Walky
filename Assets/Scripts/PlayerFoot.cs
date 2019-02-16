424a499aaa0b4f5d613b029cb5ad45d562ce4830
Kai Kuehner <kvk@pobox.com>
2019-02-16 14:27:35 -0500

    intial project files

---
+++ Assets/Scripts/PlayerFoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoot : MonoBehaviour
+{
+	
+	public enum Side
+	{
+		Left,
+		Right
+	}
+
+	public Side side;
+
+	private Rigidbody2D rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/PlayerLeg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLeg : MonoBehaviour
+{
+	public GameObject foot;
+	public GameObject head;
+
+	private LineRenderer lr;
+
+	private void Start()
+	{
+		lr = GetComponent<LineRenderer>();
+	}
+	
+	private void Update()
+	{
+		Vector3[] positions = { foot.transform.position, head.transform.position };
+		lr.SetPositions(positions);
+	}
+}
