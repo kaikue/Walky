3148c260b192614b55e642ce7888eebe2fafff53
Kai Kuehner <kvk@pobox.com>
2019-02-16 21:44:01 -0500

    walkin on sunshine wooahoah

--- Assets/Scripts/PlayerFoot.cs
+++ Assets/Scripts/PlayerFoot.cs
@@ -12,16 +12,82 @@ public class PlayerFoot : MonoBehaviour
 	}
 
 	public Side side;
+	public PlayerFoot otherFoot;
+
+	private const float SPEED = 3.0f;
 
 	private Rigidbody2D rb;
+	private FixedJoint2D fj;
+	private HashSet<GameObject> floors;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		fj = GetComponent<FixedJoint2D>();
+		floors = new HashSet<GameObject>();
 	}
 	
-	private void Update()
+	private void FixedUpdate()
 	{
-		
+		//TODO: if both fj disabled, allow movement (midair)
+
+		if (otherFoot.fj.enabled) //if other foot on ground
+		{
+			float xVel = Input.GetAxis(side + "Horizontal");
+			float yVel = Input.GetAxis(side + "Vertical");
+			Vector2 vel = new Vector2(xVel, yVel) * SPEED;
+
+			//pop off:
+			//compare vel to floor normal (dot product?)
+			//magnitude with unity input gravity for "forceful" input?
+
+			if (vel.magnitude > 0.1f)
+			{
+				fj.enabled = false;
+				floors.Clear();
+			}
+
+			rb.velocity = vel;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Collide(collision);
+	}
+
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		Collide(collision);
+	}
+
+	private void Collide(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("LevelGeometry") && !floors.Contains(collision.gameObject))
+		{
+			floors.Add(collision.gameObject);
+
+			if (collision.rigidbody == null)
+			{
+				//connect to fixed point in world
+				fj.autoConfigureConnectedAnchor = false;
+				fj.connectedAnchor = rb.position;
+			}
+			else
+			{
+				//connect to other rigidbody
+				fj.autoConfigureConnectedAnchor = true;
+				fj.connectedBody = collision.rigidbody;
+			}
+			fj.enabled = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("LevelGeometry"))
+		{
+			floors.Remove(collision.gameObject);
+		}
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Walky
+# Untitled Walky Game
+
+Experimental physics-based 2D sidescroller with unusual movement.
+
+## TODO
+
+### Movement
+- Head
+	- stay up in the air
+	- stay right side up/upside down (no spinning)
+- Legs
+	- curve (based on bezier with head sides?)
+- Feet
+	- target rotation based on nearest floor normal
+
+### Levels
+- Tilemap
+	- wall
+	- big/small outside corners
+	- big/small inside corners
+- Climb up stairs
+- Climb up to the ceiling
+- Swimming?
+
+### Mechanics
+- Collect stuff
+- NPCs?
+- Customizables?
+	- help NPCs to get their clothing item?
+
+### Sound
+- Suck, pop noise for feet unstick
+- Pop noise for feet stick
+- Noise for legs bending?
+- Collect thingy
+- Finish level
+- Talk?
+
+### Art
+- Player
+	- Feet stretch when unsticking
+- Level geometry
+	- simple solid colors
+- Backgrounds
+- Collectibles
